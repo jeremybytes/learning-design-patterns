90856ed649a06e1e9cc7c813e63673145de3cfa7
Jeremy Clark <jeremy@jeremybytes.com>
2016-05-30 12:25:29 -0700

    Updates to show Observer pattern

--- Observer/MainWindow.xaml.cs
+++ Observer/MainWindow.xaml.cs
@@ -7,6 +7,24 @@ namespace Observer
         public MainWindow()
         {
             InitializeComponent();
+            ClickMeButton.Click += Observer1;
+            ClickMeButton.Click += Observer2;
+            ClickMeButton.Click += Observer3;
+        }
+
+        private void Observer1(object sender, RoutedEventArgs e)
+        {
+            TextBlock1.Text = "Hello from Observer 1";
+        }
+
+        private void Observer2(object sender, RoutedEventArgs e)
+        {
+            TextBlock2.Text = "Hello from Observer 2";
+        }
+
+        private void Observer3(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello from Observer 3");
         }
     }
 }

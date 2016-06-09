fc5f81fa81326809c68f0fb119280621b2073d4a
Jeremy Clark <jeremy@jeremybytes.com>
2016-06-09 13:33:42 -0700

    Globalization Date Fix

--- WCFService/PersonService.svc.cs
+++ WCFService/PersonService.svc.cs
@@ -12,19 +12,19 @@ namespace WCFService
             var p = new List<Person>()
             {
                 new Person() { FirstName="John", LastName="Koenig",
-                    StartDate = DateTime.Parse("10/17/1975"), Rating=6 },
-                new Person() { FirstName="Dylan", LastName="Hunt", 
-                    StartDate = DateTime.Parse("10/02/2000"), Rating=8 },
-                new Person() { FirstName="John", LastName="Crichton", 
-                    StartDate = DateTime.Parse("03/19/1999"), Rating=7 },
-                new Person() { FirstName="Dave", LastName="Lister", 
-                    StartDate = DateTime.Parse("02/15/1988"), Rating=9 },
-                new Person() { FirstName="John", LastName="Sheridan", 
-                    StartDate = DateTime.Parse("01/26/1994"), Rating=6 },
-                new Person() { FirstName="Dante", LastName="Montana", 
-                    StartDate = DateTime.Parse("11/01/2000"), Rating=5 },
-                new Person() { FirstName="Isaac", LastName="Gampu", 
-                    StartDate = DateTime.Parse("09/10/1977"), Rating=4 }
+                    StartDate = new DateTime(1975, 10, 17), Rating=6 },
+                new Person() { FirstName="Dylan", LastName="Hunt",
+                    StartDate = new DateTime(2000, 10, 2), Rating=8 },
+                new Person() { FirstName="John", LastName="Crichton",
+                    StartDate = new DateTime(1999, 3, 19), Rating=7 },
+                new Person() { FirstName="Dave", LastName="Lister",
+                    StartDate = new DateTime(1988, 2, 15), Rating=9 },
+                new Person() { FirstName="John", LastName="Sheridan",
+                    StartDate = new DateTime(1994, 1, 26), Rating=6 },
+                new Person() { FirstName="Dante", LastName="Montana",
+                    StartDate = new DateTime(2000, 11, 1), Rating=5 },
+                new Person() { FirstName="Isaac", LastName="Gampu",
+                    StartDate = new DateTime(1977, 9, 10), Rating=4 }
             };
             return p;
         }

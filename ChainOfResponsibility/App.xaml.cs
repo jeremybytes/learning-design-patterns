a9389e049d2de4c7324c7bb20de0bc77b46c1126
Jeremy Clark <jeremy@jeremybytes.com>
2023-10-21 12:52:44 -0700

    Added global exception message to Chain of Responsibility

--- ChainOfResponsibility/App.xaml.cs
+++ ChainOfResponsibility/App.xaml.cs
@@ -4,4 +4,10 @@ namespace ChainOfResponsibility;
 
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        Application.Current.DispatcherUnhandledException += 
+            (_, _) => MessageBox.Show("UNHANDLED EXCEPTION\n\nApplication Exiting");
+    }
 }
